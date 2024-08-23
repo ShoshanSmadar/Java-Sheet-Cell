1905e35fe99f5740ad1925fdb6eb0a3679194c7a
ShoshanSmadar <Smadarshoshan@gmail.com>
2024-08-23 15:58:41 +0300

    setters getters in sheetDTO and celLDTO

--- DTO/src/cell/CellDTO.java
+++ DTO/src/cell/CellDTO.java
@@ -12,7 +12,7 @@ public class CellDTO {
     protected List<Coordinate> affectedBy;
     protected List<Coordinate> affecting;
 
-    public  CellDTO(Coordinate coordinate, String originalValue, Object effectiveValue, int lastVersionChanged, List<Coordinate> affectedBy, List<Coordinate> affecting) {
+    public CellDTO(Coordinate coordinate, String originalValue, Object effectiveValue, int lastVersionChanged, List<Coordinate> affectedBy, List<Coordinate> affecting) {
         this.coordinate = coordinate;
         this.originalValue = originalValue;
         this.effectiveValue = effectiveValue;
@@ -21,6 +21,43 @@ public class CellDTO {
         this.affecting = affecting;
     }
 
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+    public String getOriginalValue() {
+        return originalValue;
+    }
+    public Object getEffectiveValue() {
+        return effectiveValue;
+    }
+    public int getLastVersionChanged() {
+        return lastVersionChanged;
+    }
+    public List<Coordinate> getAffectedBy() {
+        return affectedBy;
+    }
+    public List<Coordinate> getAffecting() {
+        return affecting;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+    public void setOriginalValue(String originalValue) {
+        this.originalValue = originalValue;
+    }
+    public void setEffectiveValue(Object effectiveValue) {
+        this.effectiveValue = effectiveValue;
+    }
+    public void setLastVersionChanged(int lastVersionChanged) {
+        this.lastVersionChanged = lastVersionChanged;
+    }
+    public void setAffectedBy(List<Coordinate> affectedBy) {
+        this.affectedBy = affectedBy;
+    }
+    public void setAffecting(List<Coordinate> affecting) {
+        this.affecting = affecting;
+    }
 
 
 }
--- DTO/src/sheet/sheetDTO.java
+++ DTO/src/sheet/sheetDTO.java
@@ -32,20 +32,52 @@ public class sheetDTO {
     public String getSheetName() {
         return sheetName;
     }
+
     public Map<Coordinate, CellDTO> getCellMap() {
         return cellMap;
     }
+
     public int getSizeOfColumns() {
         return sizeOfColumns;
     }
+
     public int getLengthOfCol() {
         return lengthOfCol;
     }
+
     public int getSizeOfRows() {
         return sizeOfRows;
     }
+
     public int getHeightOfRow() {
         return heightOfRow;
     }
 
+    public void setCellMap(Map<Coordinate, CellDTO> cellMap) {
+        this.cellMap = cellMap;
+    }
+
+    public void setSizeOfColumns(int sizeOfColumns) {
+        this.sizeOfColumns = sizeOfColumns;
+    }
+
+    public void setLengthOfCol(int lengthOfCol) {
+        this.lengthOfCol = lengthOfCol;
+    }
+
+    public void setSizeOfRows(int sizeOfRows) {
+        this.sizeOfRows = sizeOfRows;
+    }
+
+    public void setHeightOfRow(int heightOfRow) {
+        this.heightOfRow = heightOfRow;
+    }
+
+    public void setSheetName(String sheetName) {
+        this.sheetName = sheetName;
+    }
+
+    public void setSheetVersion(int sheetVersion) {
+        this.sheetVersion = sheetVersion;
+    }
 }
